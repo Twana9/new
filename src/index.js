e3ea1b8263e275429243db7b5ddb57ee86df6c07
twana koye <twana.00569020@gmail.com>
2025-06-24 22:02:08 +0300

    day one

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
-console.log("index.js");
-console.log("version20");
+// console.log("hello world");
+// console.log("i like pezza");
+// window.alert("this is an alert");
+// window.alert("this ");
+
+document.getElementById("myh1").textContent = "twana";
+document.getElementById("myp").textContent = "see more";
