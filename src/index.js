91d766762c8ea2d4c7e761e71bc93011097fc944
twana koye <twana.00569020@gmail.com>
2025-06-26 22:15:19 +0300

    day 2

--- src/index.js
+++ src/index.js
@@ -3,5 +3,18 @@
 // window.alert("this is an alert");
 // window.alert("this ");
 
-document.getElementById("myh1").textContent = "twana";
-document.getElementById("myp").textContent = "see more";
+// let b = 5;
+// let c = 12.5;
+// let d = "twana";
+// console.log(`your age is ${aa}`);
+// console.log(`your age is ${d}`);
+// console.log(typeof c);
+// console.log(typeof d);
+// let online = true;
+// console.log(!online);
+let students = 31;
+
+// let extra = students % 2 == 0;
+// ++students;
+students -= 1;
+console.log(students);
