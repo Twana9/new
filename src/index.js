913032d62377ab8c742835dba2acdfc8f2d20e2c
Twana <twana.00569020@gmail.com>
2026-07-08 14:44:57 -0700

    dfs

--- src/index.js
+++ src/index.js
@@ -368,16 +368,26 @@
 
 // console.log(countTruthy([1, 2, 3, false, undefined, "aa"]));
 
-const movie = {
-  title: "a",
-  releaseYear: 2018,
-  rating: 4.5,
-  director: "b",
-};
+// const movie = {
+//   title: "a",
+//   releaseYear: 2018,
+//   rating: 4.5,
+//   director: "b",
+// };
 
-function showProperties(movie) {
-  for (prop in movie) {
-    if (typeof movie[prop] === "string") console.log(prop, movie[prop]);
-  }
+// function showProperties(movie) {
+//   for (prop in movie) {
+//     if (typeof movie[prop] === "string") console.log(prop, movie[prop]);
+//   }
+// }
+// showProperties(movie);
+
+console.log(sum(10));
+
+function sum(limit) {
+  let sum = 0;
+  for (let i = 0; i <= limit; i++)
+    if (i % 3 === 0 || i % 5 === 0) sum = sum + i;
+
+  return sum;
 }
-showProperties(movie);
