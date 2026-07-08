68a73fa23afda829ac167cd104a223849f0333b1
Twana <twana.00569020@gmail.com>
2026-07-08 14:16:49 -0700

    dfd

--- src/index.js
+++ src/index.js
@@ -354,3 +354,16 @@
 //   }
 // }
 // showNumbers(10);
+
+function countTruthy(array) {
+  let count = 0;
+  for (let item of array) {
+    if (item) {
+      count++;
+      console.log(item);
+    }
+  }
+  return count;
+}
+
+console.log(countTruthy([1, 2, 3, false, undefined, "aa"]));
