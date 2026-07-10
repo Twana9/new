b185e76b8912be84987ffd09ed8dc5e4b0980d4a
Twana <twana.00569020@gmail.com>
2026-07-10 15:17:06 -0700

    sdf

--- src/index.js
+++ src/index.js
@@ -407,15 +407,17 @@
 //     console.log(point);
 //   }
 // }
-checkSpeed(129);
+checkSpeed(800);
 function checkSpeed(speed) {
   const speedLimit = 70;
-  const pointValue = 5;
-  if (speed < speedLimit + pointValue) console.log("ok");
-  else {
-    const point = Math.floor((speed - speedLimit) / 5);
-
-    if (point >= 12) console.log("License Suspended");
-    else console.log("Point: ", point);
+  const kmPerPoint = 5;
+  if (speed < speedLimit + kmPerPoint) {
+    console.log("ok");
+    return;
   }
+
+  const point = Math.floor((speed - speedLimit) / kmPerPoint);
+
+  if (point >= 12) console.log("License Suspended");
+  else console.log("Point: ", point);
 }
