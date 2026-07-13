8faaf6a186011e89e50f3822052537b9adbea5bf
Twana <twana.00569020@gmail.com>
2026-07-13 16:30:50 -0700

    sd

--- src/index.js
+++ src/index.js
@@ -471,3 +471,15 @@
 
 //   return true;
 // }
+const circle = {
+  radius: 1,
+  location: {
+    x: 1,
+    y: 1,
+  },
+  isVisible: true,
+  draw: function () {
+    console.log("draw");
+  },
+};
+circle.draw();
