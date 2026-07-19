184d7a3e3091a44e0181a7214e31ceb4a7ae7e7f
Twana <twana.00569020@gmail.com>
2026-07-19 10:06:12 -0700

    sf

--- src/index.js
+++ src/index.js
@@ -544,20 +544,35 @@
 // }
 // showAddress(address);
 
-function Address2(street, city, zipCode) {
-  return {
-    street,
-    city,
-    zipCode,
-  };
-}
-const fac = new Address2("shahidan", "koya", 99);
-console.log(fac);
+// function Address2(street, city, zipCode) {
+//   return {
+//     street,
+//     city,
+//     zipCode,
+//   };
+// }
+// const fac = new Address2("shahidan", "koya", 99);
+// console.log(fac);
 
-function Address1(street, city, zipCode) {
+function Address(street, city, zipCode) {
   this.street = street;
   this.city = city;
   this.zipCode = zipCode;
 }
-const add = new Address1("shahidan", "koya", 99);
-console.log(add);
+const address1 = new Address("shahidan", "koya", 99);
+const address2 = new Address("shahidan", "koya", 99);
+const address3 = address1;
+
+function areEqual(address1, address2) {
+  return (
+    address1.street === address2.street &&
+    address1.city === address2.city &&
+    address1.zipCode === address2.zipCode
+  );
+}
+function areSame(address1, address2) {
+  return address1 === address2;
+}
+console.log(areEqual(address1, address2));
+console.log(areSame(address1, address2));
+console.log(areSame(address1, address3));
