c2b287eee0e618a8360af64ed11d7e0f03e75cf0
Twana <twana.00569020@gmail.com>
2026-07-27 11:11:58 -0700

    afdasf

--- src/index.js
+++ src/index.js
@@ -895,16 +895,9 @@ function sum(...numbers) {
 
 const circle = {
   radius: 1,
-
-  // get radius() {
-  //   return this.radius;
-  // },
-  // set radius(value) {
-  //   this.radius = value;
-  // },
   get area() {
     return Math.PI * this.radius * this.radius;
   },
 };
-// circle.radius = 2;
+circle.radius = 2;
 console.log(circle.area);
