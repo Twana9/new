7b59ae7b9366abefb85081770f3d7758ae32e1a1
Twana <twana.00569020@gmail.com>
2026-07-30 07:59:25 -0700

    method overriding

--- src/index.js
+++ src/index.js
@@ -998,21 +998,35 @@
 // const c = new Circle(1);
 // console.log(c);
 
+// class Shape {
+//   constructor(color) {
+//     this.color = color;
+//   }
+//   move() {
+//     console.log("move");
+//   }
+// }
+// class Circle extends Shape {
+//   constructor(color, radius) {
+//     super(color);
+//     this.radius = radius;
+//   }
+//   draw() {
+//     console.log("draw");
+//   }
+// }
+// const c = new Circle("red", 1);
+
 class Shape {
-  constructor(color) {
-    this.color = color;
-  }
   move() {
     console.log("move");
   }
 }
 class Circle extends Shape {
-  constructor(color, radius) {
-    super(color);
-    this.radius = radius;
-  }
-  draw() {
-    console.log("draw");
+  move() {
+    super.move();
+    console.log("move circle");
   }
 }
-const c = new Circle("red", 1);
+const c = new Circle();
+const b = new Shape();
